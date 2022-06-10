2a5adb501dce7389e7faea3c69790e9ba97fe5a8
Giovanni Ciotta <giociotta@gmail.com>
2022-06-10 17:54:31 +0200

    stampo card album

---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,23 @@
+<template>
+ <div class="card">
+    <img :src="datails.poster" :alt="datails.title">
+    <h4> {{datails.title}} </h4>
+    <span> {{datails.author}} </span><br>
+    <span> {{datails.year}} </span>
+ </div>
+ 
+</template>
+
+<script>
+export default {
+   name: 'AlbumCard',
+   props:{
+    datails:Object
+   } 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
--- src/components/DischiList.vue
+++ src/components/DischiList.vue
@@ -1,17 +1,21 @@
 <template>
   <section>
-    qui ci vanno le card
+  <AlbumCard
+    v-for="(item, index) in listaAlbum" :key="index"
+    :datails="item"
+  />
   </section>
 </template>
 
 <script>
 
 import axios from "axios"
+import AlbumCard from '@/components/AlbumCard.vue'
 
 export default {
   name: 'DischiList',
- components: {
-    
+   components: {
+  AlbumCard
   },
   data(){
     return{
@@ -21,11 +25,17 @@ export default {
   },
 
   created(){
-    axios.get(this.apiUrl)
-    .then((result)=>{
+    this.getAlbum()
+  },
+
+  methods:{
+    getAlbum(){
+        axios.get(this.apiUrl)
+        .then((result)=>{
         this.listaAlbum = result.data;
         console.log(result);
     })
+    }
   }
 }
 </script>
@@ -35,5 +45,7 @@ export default {
 section{
     height: calc(100vh - 5vh);
     background-color: #1e2d3b;
+    display: flex;
+    justify-content: space-between;
 }
 </style>
